09ebbdcbf71d351df2e906c59519fefed2a55a4d
Raquel Barbosa Romão <raquelbromao@gmail.com>
2021-05-25 07:29:32 -0300

    first commit

---
+++ src/test/java/star/wars/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package star.wars.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
